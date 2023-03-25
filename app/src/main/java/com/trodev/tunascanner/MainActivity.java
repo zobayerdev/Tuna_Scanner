de0934f5d7927c578d451a79b857a206a71e05a1
zobayerdev <zobayer.dev@gmail.com>
2023-03-25 17:33:52 +0600

    design are complete -nayem

--- app/src/main/java/com/trodev/tunascanner/MainActivity.java
+++ app/src/main/java/com/trodev/tunascanner/MainActivity.java
@@ -1,14 +1,193 @@
 package com.trodev.tunascanner;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView bottomNavigationView;
+
+    private Button go_notification_activity;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    private CardView member, video_call, notice, calls, motives, location, sms, gallery, profile;
+    private long pressedTime;
+    private TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navigation_view);
+
+
+        // #######################
+        // Drawer Layout implement
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // #################################################################
+        // eikhane eituku hocche amader navigation layout er kaj korar jonno.
+        navigationView.setNavigationItemSelectedListener(this::onOptionsItemSelected);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        loadHomeFragment();
+
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+                if (itemId == R.id.bottom_menu_image) {
+                    loadImagesFragment();
+                } else if (itemId == R.id.bottom_menu_home) {
+                    loadHomeFragment();
+                } else if (itemId == R.id.bottom_menu_scanner) {
+                    loadScannerFragment();
+                } else if (itemId == R.id.bottom_menu_pdf) {
+                    loadPDFFragment();
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid Click", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
     }
+
+    private void loadImagesFragment() {
+        setTitle("Images");
+        ImageListFragment imageListFragment = new ImageListFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, imageListFragment, "ImageListFragment");
+        fragmentTransaction.commit();
+    }
+
+    private void loadPDFFragment() {
+        setTitle("PDFs");
+        pdfListFragment pdfListFragment = new pdfListFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, pdfListFragment, "PdfListFragment");
+        fragmentTransaction.commit();
+    }
+
+
+    private void loadHomeFragment() {
+        setTitle("Home");
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, homeFragment, "HomeListFragment");
+        fragmentTransaction.commit();
+    }
+
+    private void loadScannerFragment() {
+        setTitle("Scanner");
+        CameraFragment cameraFragment = new CameraFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, cameraFragment, "ScannerListFragment");
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        switch (item.getItemId()) {
+            case R.id.nav_notification_notice:
+                Toast.makeText(this, "নোটিফিকেশান!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_dev:
+                final Dialog dialog = new Dialog(this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.developer_bottomsheet_layout);
+
+                dialog.show();
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+                dialog.getWindow().setGravity(Gravity.BOTTOM);
+                Toast.makeText(this, "ডেভেলপার পরিচয়!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_policy:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.app-privacy-policy.com/live.php?token=qB3iS10fUJmr6yEFtaVo9yve0uuPP3Ok")));
+                Toast.makeText(this, "প্রাইভেসি পলিসি!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_share:
+                try {
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, "The Holy Quran -Islamic App");
+                    String shareMessage = "\nThe Holy Quran -Islamic App অ্যাপটি ডাউনলোড করুন\n\n";
+                    shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "\n\n";
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+                    startActivity(Intent.createChooser(shareIntent, "choose one"));
+                    Toast.makeText(this, "শেয়ার করুন!", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    //e.toString();
+                }
+                break;
+            case R.id.nav_rate:
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + getPackageName())));
+                    Toast.makeText(this, "রেটিং দিন!", Toast.LENGTH_SHORT).show();
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName())));
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    // In this code, android lifecycle exit on 2 times back-pressed.
+    @Override
+    public void onBackPressed() {
+
+        if (pressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+        } else {
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        pressedTime = System.currentTimeMillis();
+    }
+
 }
