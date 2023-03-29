2e474e26b6bd4ed08c1e33b8c1cf96b2174d3dcf
zobayerdev <zobayer.dev@gmail.com>
2023-03-30 02:01:52 +0600

    complete pdf rename, delete, share -nayem

---
+++ app/src/main/java/com/trodev/tunascanner/activities/ScanGalleryActivity.java
@@ -0,0 +1,14 @@
+package com.trodev.tunascanner.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScanGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_gallery);
+    }
+}
